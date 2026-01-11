d378aa8f91c795af81aa948995e697096b84a804
MieMieeeee <llsheepmie@gmail.com>
2026-01-11 13:49:08 +0800

    feat(prompt_generator): 添加LTX2PromptGenerator和图像转数据URL功能
    - 新增LTX2视频生成模型的提示词生成器
    - 添加image_tensor_to_data_url工具函数用于图像转base64数据URL
    - 更新.gitignore忽略mie_llm_keys.json文件

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from .caption_file_operator import BatchRenameFiles, BatchDeleteFiles, BatchEdit
 from .downloader import ModelDownloader
 from .translator import TextTranslator
 from .prompt_generator import PromptGenerator, KontextPromptGenerator, AddUserKontextPreset, RemoveUserKontextPreset, \
-    FrameTransitionPromptGenerator, HunyuanVideoI2VPromptGenerator, HunyuanVideoT2VPromptGenerator, ZImagePromptGenerator, Flux2PromptGenerator
+    FrameTransitionPromptGenerator, HunyuanVideoI2VPromptGenerator, HunyuanVideoT2VPromptGenerator, ZImagePromptGenerator, Flux2PromptGenerator, LTX2PromptGenerator
 from .llm_service_connector import SetGeneralLLMServiceConnector, SetSiliconFlowLLMServiceConnector, \
     SetGithubModelsLLMServiceConnector, SetZhiPuLLMServiceConnector, SetKimiLLMServiceConnector, \
     SetDeepSeekLLMServiceConnector, SetGeminiLLMServiceConnector, SetBailianLLMServiceConnector, \
@@ -49,6 +49,7 @@ NODE_CLASS_MAPPINGS = {
     add_suffix("HunyuanVideoT2VPromptGenerator"): HunyuanVideoT2VPromptGenerator,
     add_suffix("ZImagePromptGenerator"): ZImagePromptGenerator,
     add_suffix("Flux2PromptGenerator"): Flux2PromptGenerator,
+    add_suffix("LTX2PromptGenerator"): LTX2PromptGenerator,
     add_suffix("GetAbsolutePath"): GetAbsolutePath,
     add_suffix("GetFileInfo"): GetFileInfo,
     add_suffix("GetDirectoryFilesInfo"): GetDirectoryFilesInfo,
@@ -93,6 +94,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     add_suffix("HunyuanVideoT2VPromptGenerator"): add_emoji("Hunyuan Video T2V Prompt Generator"),
     add_suffix("ZImagePromptGenerator"): add_emoji("Z-Image Prompt Generator"),
     add_suffix("Flux2PromptGenerator"): add_emoji("Flux2 Prompt Generator"),
+    add_suffix("LTX2PromptGenerator"): add_emoji("LTX2 Prompt Generator"),
     add_suffix("GetAbsolutePath"): add_emoji("Get Absolute Path"),
     add_suffix("GetFileInfo"): add_emoji("Get File Info"),
     add_suffix("GetDirectoryFilesInfo"): add_emoji("Get Directory Files Info"),
